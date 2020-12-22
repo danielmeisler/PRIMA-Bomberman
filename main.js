9f66e25002c9a3da5407e4dce081e1036b9ea73f
Daniel <daniel_meisler@web.de>
2020-12-22 14:07:13 +0100

    Repository eingerichtet

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+var Bomberman;
+(function (Bomberman) {
+    console.log("Test");
+})(Bomberman || (Bomberman = {}));
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,3 @@
+namespace Bomberman {
+  console.log("Test");
+}
